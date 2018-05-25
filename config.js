5bdc2a3992063ffa936ddb2e12d7f0b08e388371
lilyhdo <lily4190@yahoo.com>
2018-05-25 10:05:27 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+export const USERNAME = process.env.KOBITON_USERNAME || 'kobiton_username'
+export const API_KEY = process.env.KOBITON_API_KEY || 'kobiton_api_key'
+export const DEVICE_NAME = process.env.KOBITON_DEVICE_NAME || 'kobiton_device_name'
+export const PLATFORM_NAME = process.env.KOBITON_PLATFORM_NAME || 'kobiton_platform_name'
