998c01b8bb3d8a6ec7014dba554529f6d7c76c7f
thangvo38 <thangvo8797@gmail.com>
2018-08-04 22:28:24 +0700

    Fix some stuff

--- api-request.js
+++ api-request.js
@@ -2,26 +2,29 @@ import 'babel-polyfill'
 import 'colors'
 import fs from 'fs'
 import request from 'request'
+import {USERNAME, API_KEY} from './config'
 
-const username = configs.USERNAME
-const apiKey = configs.API_KEY 
+const ROOT_DIR = '/Users/thangvo/Downloads'
+const FILE_NAME = "KMSDirectory_2_2.6.apk"
+const APK_DIR = `${ROOT_DIR}/${FILE_NAME}`
+const username = USERNAME
+const apiKey = API_KEY 
 
-const filename = "KMSDirectory_2_2.6.apk"
 var basicAuth = "Basic " + new Buffer(username + ":" + apiKey).toString("base64");
-
+console.log(`Params: ${username} - ${apiKey}`)
 main()
 
 function main() {
-  generateUrl(function (url) {
-  UploadToS3(url)
-  createAppVer()
+  generateUrl(function (url, path) {
+  UploadToS3(url, path)
   })
 }
 
 function generateUrl(callback) {
   const inputBody = {
-    "filename": filename,
-    "appId": 3148
+    //Uncomment the line below if you want to upload app instead of creating a new one
+    //"addId": ???,
+    "filename": FILE_NAME
   };
   const headers = {
     "Authorization": `${basicAuth}`,
@@ -33,21 +36,21 @@ function generateUrl(callback) {
   var result = ''
 
   request({
-    url: 'https://api-test.kobiton.com/v1/apps/uploadUrl',
+    url: 'https://api.kobiton.com/v1/apps/uploadUrl',
     json: true,
     method: 'POST',
     body: inputBody,
     headers: headers
   }, function (err, response, body) {
     if (err) return console.error('Error:', err);
-    console.log(body.url)
-    callback(body.url)
+    console.log('GENERATE URL DONE')
+    callback(body.url, body.appPath) //<---- Have to get the appPath here
   })
 }
 
-function UploadToS3(url) {
+function UploadToS3(url, appPath) {
   console.log('Flow can come here')
-  var stats = fs.statSync(`/Users/lilydo/Downloads/${filename}`);
+  var stats = fs.statSync(APK_DIR);
   const option = {
     method: 'PUT',
     url: url,
@@ -58,21 +61,21 @@ function UploadToS3(url) {
     }
   }
 
-  console.log('check:', option)
-  fs.createReadStream(`/Users/lilydo/Downloads/${filename}`).pipe(request(option
+  fs.createReadStream(APK_DIR).pipe(request(option
     , function (err, res) {
     if (err) {
       console.log('error:',err)
     }
-    console.log('result: ', res.statusCode)
+    console.log('UPLOAD TO S3 DONE')
+    createAppVer(appPath) //<---- Create app versioin on Kobiton after uploading to S3
   }));
 }
 
-function createAppVer() {
+function createAppVer(appPath) {
   const request = require('request');
   const inputBody = {
-    "filename": filename,
-    "appPath": "users/10331/apps/KMSDirectory_2_2.6-75085930-7823-11e8-b694-21deefd115a4.apk"
+    "filename": FILE_NAME,
+    "appPath": appPath
   };
   const headers = {
     'Authorization': basicAuth,
@@ -80,7 +83,7 @@ function createAppVer() {
   };
 
   request({
-    url: 'https://api-test.kobiton.com/v1/apps',
+    url: 'https://api.kobiton.com/v1/apps',
     json: true,
     method: 'POST',
     body: inputBody,
@@ -88,7 +91,7 @@ function createAppVer() {
   }, function (err, response, body) {
     if (err) return console.error('Error:', err);
 
-    // console.log('Response:', response);
+    console.log('App has successfully uploaded to Kobiton')
     console.log('Response body:', body);
   });
 }
